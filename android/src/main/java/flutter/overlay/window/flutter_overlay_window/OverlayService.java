4d58a5681f4dd215dae4af6d29cefefc3537251a
CiceroTecchio <cicero_tecchio@hotmail.com>
2025-06-18 17:29:05 -0300

    Update OverlayService.java

--- android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
+++ android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
@@ -353,7 +353,7 @@ public class OverlayService extends Service implements View.OnTouchListener {
         Notification notification = new NotificationCompat.Builder(this, OverlayConstants.CHANNEL_ID)
                 .setContentTitle(WindowSetup.overlayTitle)
                 .setContentText(WindowSetup.overlayContent)
-               .setSmallIcon(notifyIcon == 0 ? R.drawable.ic_launcher_notification : notifyIcon)
+               .setSmallIcon(notifyIcon == 0 ? R.drawable.notification_icon : notifyIcon)
                 .setContentIntent(pendingIntent)
                 .setVisibility(WindowSetup.notificationVisibility)
                 .build();
