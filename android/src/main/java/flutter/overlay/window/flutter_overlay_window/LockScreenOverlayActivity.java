31f8d40dcbeeedc8773254f1f396b43724242eb2
CiceroTecchio <cicero_tecchio@hotmail.com>
2025-07-31 18:53:23 -0300

    Update LockScreenOverlayActivity.java

--- android/src/main/java/flutter/overlay/window/flutter_overlay_window/LockScreenOverlayActivity.java
+++ android/src/main/java/flutter/overlay/window/flutter_overlay_window/LockScreenOverlayActivity.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Handler;
 
 import android.app.Activity;
 import android.os.Build;
