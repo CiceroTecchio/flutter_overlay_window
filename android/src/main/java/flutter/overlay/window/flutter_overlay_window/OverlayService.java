fa9172a853b7ae36552bd9e7119113c583e096cd
CiceroTecchio <cicero_tecchio@hotmail.com>
2025-08-08 21:10:12 -0300

    Update OverlayService.java

--- android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
+++ android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
@@ -172,9 +172,7 @@ public class OverlayService extends Service implements View.OnTouchListener {
         } else if ("RESUME_OVERLAY".equals(action)) {
             FlutterEngine flutterEngine = FlutterEngineCache.getInstance().get(OverlayConstants.CACHED_TAG);
             if (flutterView != null && flutterEngine != null) {
-                flutterView.attachToFlutterEngine(flutterEngine);
-                flutterView.invalidate();
-                Log.d("OverlayService", "FlutterView resumido e redraw feito.");
+                flutterView.post(() -> flutterView.invalidate());
             } else {
                 Log.w("OverlayService", "flutterView ou flutterEngine nulos ao tentar resumir.");
             }
